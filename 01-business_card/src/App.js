38128045c88b974fe04620ae2583149946d2ab5d
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-03-18 18:20:57 -0300

    up

--- 01-business_card/src/App.js
+++ 01-business_card/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Header from "../src/content/Header";
+import MainContent from"../src/content/MainContent";
+import Footer from "../src/content/Footer";
 
 export default function App(){
     return (
-        <div>
-            <h1>Teste de função</h1>
-        </div>
+        <main>
+            <Header />
+            <MainContent />
+            <Footer />
+        </main>
+
     )
 }

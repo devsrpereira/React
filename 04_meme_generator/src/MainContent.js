d604ba93640a44a33c3d1b34a4f9e013bf24ec15
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-03-23 21:01:38 -0300

    up

--- 04_meme_generator/src/MainContent.js
+++ 04_meme_generator/src/MainContent.js
@@ -3,14 +3,14 @@ import React from 'react'
 export default function MainContent(){
     return(
         <section className="main_content">
-            <form action='#' method='get'>
-                <span className="form_text">
-                    <input type="text" />
-                    <input type="text" />
+            <form className="main_form" action='#' method='get'>
+                <span className="form_texts">
+                    <input className="form_text" type="text" />
+                    <input className="form_text" type="text" />
                 </span>
                 <input className="form_button"type="submit" value="Get a new Meme image!" />
             </form>
-            <img></img>
+                 <img className="main_img" src='../images/img_01.png'></img>
 
         </section>
     )

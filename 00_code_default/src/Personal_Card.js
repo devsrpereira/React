0fb147e8d76cad4652a0d3211bb680184b996251
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-03-26 18:08:10 -0300

    up

--- 00_code_default/src/Personal_Card.js
+++ 00_code_default/src/Personal_Card.js
@@ -7,20 +7,31 @@ export default function Personal_Card(){
         lastName: "Pereira",
         phone: "+55 (11) 994104833",
         email: "srdevpereira@gmail.com",
-        isFavorit: false
+        picture:"foto_perfil01.jpg",
+        isFavorit: false,
     })
 
+    function put_favorit(){
+        setContact.isFavorit(prevState => !prevState)
+        
+    }
+    
+    
 
 return(
     <div className='card_page'>
         <div className='card_picture'>
-            <img src='https://1drv.ms/i/s!AjZLahQvN68hfAOkEUCcJ8ByCRQ?e=aIuLQr' />
+            <img className='picture_img' src= {`../images/${contact.picture}`} />
         </div>
         <div className ='card_content'>
-            <p className ='content_star'><ion-icon name="star-outline"></ion-icon></p>
-            <h1 className ='content_name'>NAME</h1>
-            <p className ='content_phone'>PHONE NUMBER</p>
-            <p className ='content_email'>EMAIL</p>
+            <p className ='content_star' onClick={put_favorit}><ion-icon 
+            name={contact.isFavotit ? "star" : "star-outline"}></ion-icon></p>
+
+           
+
+            <h1 className ='content_name'>{`${contact.firstName} ${contact.lastName}`}</h1>
+            <p className ='content_phone'>{contact.phone} </p>
+            <p className ='content_email'>{contact.email}</p>
         </div>
     </div>
 

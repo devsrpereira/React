350a5d8b008bf775cca0d8e64f36a651978155f7
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-03-28 00:17:54 -0300

    up

--- 00_code_default/src/Box.js
+++ 00_code_default/src/Box.js
@@ -4,15 +4,16 @@ import BoxSquare from './BoxSquare'
 
 export default function Box(){
     const [square, setBox] = React.useState(box_data)
+
     
-    const on_off = square.map(item => (
-       <BoxSquare  key = {item.id} on ={item.on}/>
+    const elemento_criado = square.map(square => (
+       <BoxSquare  key = {square.id} on = {square.on} />
     ))
     
     return(
         <main className='box_page'>
             <h1 className='box_title'>Boxes will go here!</h1>
-            {on_off}
+            {elemento_criado}
         </main>
     )
 }
--- 00_code_default/src/BoxSquare.js
+++ 00_code_default/src/BoxSquare.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import Box from './Box'
 
 export default function BoxSquare(props){
+    const [on, setOn] = React.useState(props.on)
 
+    
+    
     const styles = {
-        backgroundColor: props.on ? "#222222" : "#none"
+        backgroundColor: on ? "#222222" : "trasparent",
+        cursor: "pointer",
+    }
+    
+    function changecolor(){
+        setOn(prevOn => !prevOn)
     }
 
     return(
-        <div style={styles} className='botao'></div>
+        <div style={styles} className='botao' onClick={changecolor}></div>
     )
 }

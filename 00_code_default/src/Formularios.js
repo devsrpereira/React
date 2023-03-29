5393c8196bd12f807272f2c67df7c905ad062057
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-03-29 19:20:59 -0300

    up

--- 00_code_default/src/Formularios.js
+++ 00_code_default/src/Formularios.js
@@ -3,35 +3,129 @@ import React from 'react'
 export default function Formularios(){
 
     const [formData, setformData] = React.useState(
-        {firstName:"", lastName:""}
+        {
+            firstName:"",
+            lastName:"",
+            eMail:"",
+            coMments:"",
+            isFriendly: false,
+            employment: ""
+        }
     )
 
+    console.log(formData.favColor)
+        
     function handleChange(event){
+        const{name, value, type, checked, text} = event.target
         setformData(prevformData => {
             return {
                 ...prevformData,
-                [event.target.name]: event.target.value
+                 [name]: type === "checkbox" ? checked : type === "textarea" ? text : value
             }
         })    
     }    
 
     return(
-        <div>
-        <form>
+        <div className='form_page'>
+        <form className='form_all'>
             <input
                 type ='text'
+                name = 'firstName'
                 placeholder = 'First Name'
                 onChange = {handleChange}
-                name = 'firstName'
+                value = {formData.firstName}
             />    
             <input
                 type ='text'
+                name = 'lastName'
                 placeholder = 'last Name'
                 onChange = {handleChange}
-                name = 'lastName'
-            />   
+                value = {formData.lastName}
+            />
+            <input
+                type ='email'
+                name = 'eMail'
+                placeholder = 'email@email.com'
+                onChange = {handleChange}
+                value = {formData.eMail}
+            />
+            <br/>
+            <textarea 
+                type ='textarea'
+                name = 'coMments'  
+                cols = {30}
+                rows = {10}
+                text = {formData.coMments}
+            />
+            <br/>
+            <input
+                type ='checkbox'
+                name = 'isFriendly'
+                className='no_margin'
+                id = 'isFriendly'
+                onChange = {handleChange}
+                checked = {formData.isFriendly}
+            />
+            <label htmlFor = 'isFriendly'>Are you friendly?</label>
+            <br/>
+            <fieldset>
+                <legend>Current employment status</legend>
+                <input
+                    type = 'radio'
+                    name = 'employment'
+                    className='no_margin'
+                    id = 'unemployed'
+                    value = 'unemployed'
+                    onChange = {handleChange}
+                    checked = {formData.employment === 'unemployed'}
+                />
+                <label htmlFor='unemployed'>Unemployed</label>
+                <br/>
+                <input
+                    type = 'radio'
+                    name = 'employment'
+                    className='no_margin'
+                    id = 'part-time'
+                    value = 'part-time'
+                    onChange = {handleChange}
+                    checked = {formData.employment === 'part-time'}
+                />
+                <label htmlFor='part-time'>Part-time</label>
+                <br/>
+                <input
+                    type = 'radio'
+                    name = 'employment'
+                    className='no_margin'
+                    id = 'full-time'
+                    value = 'full-time'
+                    onChange = {handleChange}
+                    checked = {formData.employment === 'full-time'}
+                />
+                <label htmlFor='full-time'>Full-time</label>
+                <br/>
+            </fieldset>
+            <br/>
+            <label htmlFor = 'favColor'>What is your favorit color?</label><br/>
+            <select
+                id = 'favColor'
+                value = {formData.favColor}
+                onChange = {handleChange}
+                name = 'favColor'
+            >
+                <option value = '' >-- Choose --</option>
+                <option value = 'red' >Red</option>
+                <option value = 'orange' >Orange</option>
+                <option value = 'green' >Green</option>
+                <option value = 'blue' >Blue</option>
+                <option value = 'indigo' >Indigo</option>
+                <option value = 'violet' >Violet</option>
+            </select>
+
+             
         </form>
             <h1>{formData.firstName} {formData.lastName}</h1>
+            <p>{formData.eMail}</p>
+            <h1>{formData.coMments}</h1>
         </div>
     )
 }

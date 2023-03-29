fa0833affcfe580bd61b7f8133aaf7c67d58e94d
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-03-29 19:59:07 -0300

    Update Formularios.js

--- 00_code_default/src/Formularios.js
+++ 00_code_default/src/Formularios.js
@@ -13,8 +13,6 @@ export default function Formularios(){
         }
     )
 
-    console.log(formData.favColor)
-        
     function handleChange(event){
         const{name, value, type, checked, text} = event.target
         setformData(prevformData => {
@@ -25,107 +23,112 @@ export default function Formularios(){
         })    
     }    
 
+    function handleSubmit(event){
+        event.preventDefault()
+        // submitToApi(formData)
+        console.log(formData)
+    }
+
     return(
         <div className='form_page'>
-        <form className='form_all'>
-            <input
-                type ='text'
-                name = 'firstName'
-                placeholder = 'First Name'
-                onChange = {handleChange}
-                value = {formData.firstName}
-            />    
-            <input
-                type ='text'
-                name = 'lastName'
-                placeholder = 'last Name'
-                onChange = {handleChange}
-                value = {formData.lastName}
-            />
-            <input
-                type ='email'
-                name = 'eMail'
-                placeholder = 'email@email.com'
-                onChange = {handleChange}
-                value = {formData.eMail}
-            />
-            <br/>
-            <textarea 
-                type ='textarea'
-                name = 'coMments'  
-                cols = {30}
-                rows = {10}
-                text = {formData.coMments}
-            />
-            <br/>
-            <input
-                type ='checkbox'
-                name = 'isFriendly'
-                className='no_margin'
-                id = 'isFriendly'
-                onChange = {handleChange}
-                checked = {formData.isFriendly}
-            />
-            <label htmlFor = 'isFriendly'>Are you friendly?</label>
-            <br/>
-            <fieldset>
-                <legend>Current employment status</legend>
+            <form className='form_all' onSubmit={handleSubmit}>
                 <input
-                    type = 'radio'
-                    name = 'employment'
-                    className='no_margin'
-                    id = 'unemployed'
-                    value = 'unemployed'
+                    type ='text'
+                    name = 'firstName'
+                    placeholder = 'First Name'
                     onChange = {handleChange}
-                    checked = {formData.employment === 'unemployed'}
+                    value = {formData.firstName}
+                />    
+                <input
+                    type ='text'
+                    name = 'lastName'
+                    placeholder = 'last Name'
+                    onChange = {handleChange}
+                    value = {formData.lastName}
                 />
-                <label htmlFor='unemployed'>Unemployed</label>
+                <input
+                    type ='email'
+                    name = 'eMail'
+                    placeholder = 'email@email.com'
+                    onChange = {handleChange}
+                    value = {formData.eMail}
+                />
+                <br/>
+                <textarea 
+                    type ='textarea'
+                    name = 'coMments'  
+                    cols = {30}
+                    rows = {10}
+                    text = {formData.coMments}
+                />
                 <br/>
                 <input
-                    type = 'radio'
-                    name = 'employment'
+                    type ='checkbox'
+                    name = 'isFriendly'
                     className='no_margin'
-                    id = 'part-time'
-                    value = 'part-time'
+                    id = 'isFriendly'
                     onChange = {handleChange}
-                    checked = {formData.employment === 'part-time'}
+                    checked = {formData.isFriendly}
                 />
-                <label htmlFor='part-time'>Part-time</label>
+                <label htmlFor = 'isFriendly'>Are you friendly?</label>
                 <br/>
-                <input
-                    type = 'radio'
-                    name = 'employment'
-                    className='no_margin'
-                    id = 'full-time'
-                    value = 'full-time'
+                <fieldset>
+                    <legend>Current employment status</legend>
+                    <input
+                        type = 'radio'
+                        name = 'employment'
+                        className='no_margin'
+                        id = 'unemployed'
+                        value = 'unemployed'
+                        onChange = {handleChange}
+                        checked = {formData.employment === 'unemployed'}
+                    />
+                    <label htmlFor='unemployed'>Unemployed</label>
+                    <br/>
+                    <input
+                        type = 'radio'
+                        name = 'employment'
+                        className='no_margin'
+                        id = 'part-time'
+                        value = 'part-time'
+                        onChange = {handleChange}
+                        checked = {formData.employment === 'part-time'}
+                    />
+                    <label htmlFor='part-time'>Part-time</label>
+                    <br/>
+                    <input
+                        type = 'radio'
+                        name = 'employment'
+                        className='no_margin'
+                        id = 'full-time'
+                        value = 'full-time'
+                        onChange = {handleChange}
+                        checked = {formData.employment === 'full-time'}
+                    />
+                    <label htmlFor='full-time'>Full-time</label>
+                    <br/>
+                </fieldset>
+                <br/>
+                <label htmlFor = 'favColor'>What is your favorit color?</label><br/>
+                <select
+                    id = 'favColor'
+                    value = {formData.favColor}
                     onChange = {handleChange}
-                    checked = {formData.employment === 'full-time'}
-                />
-                <label htmlFor='full-time'>Full-time</label>
+                    name = 'favColor'
+                >
+                    <option value = '' >-- Choose --</option>
+                    <option value = 'red' >Red</option>
+                    <option value = 'orange' >Orange</option>
+                    <option value = 'green' >Green</option>
+                    <option value = 'blue' >Blue</option>
+                    <option value = 'indigo' >Indigo</option>
+                    <option value = 'violet' >Violet</option>
+                </select>
                 <br/>
-            </fieldset>
-            <br/>
-            <label htmlFor = 'favColor'>What is your favorit color?</label><br/>
-            <select
-                id = 'favColor'
-                value = {formData.favColor}
-                onChange = {handleChange}
-                name = 'favColor'
-            >
-                <option value = '' >-- Choose --</option>
-                <option value = 'red' >Red</option>
-                <option value = 'orange' >Orange</option>
-                <option value = 'green' >Green</option>
-                <option value = 'blue' >Blue</option>
-                <option value = 'indigo' >Indigo</option>
-                <option value = 'violet' >Violet</option>
-            </select>
-
-             
-        </form>
-            <h1>{formData.firstName} {formData.lastName}</h1>
-            <p>{formData.eMail}</p>
-            <h1>{formData.coMments}</h1>
+                <input type = 'submit' value = 'Submit' />
+                
+            </form>
+            
         </div>
     )
 }

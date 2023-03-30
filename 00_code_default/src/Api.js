4993fa5a1a15899cbae5de5cbf1e332550057b64
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-03-30 17:56:06 -0300

    up

--- 00_code_default/src/Api.js
+++ 00_code_default/src/Api.js
@@ -2,16 +2,21 @@ import React from 'react'
 
 export default function Api(){
     const [starWarsData, setStarWarsData] = React.useState({})
-
-    fetch('https://swapi.dev/api/people/1')
-        .then(res => res.json())
-        .then(data => setStarWarsData(data))
+    const[count, setCount] = React.useState(0)
+    console.log('Component rendered')
 
 
+    React.useEffect(() => {
+        console.log('Effect function ran')    
+    },[count])
+
+   
     return(
         <section className='api_body'>
             <div className='api_page'>
                 <pre className='api_data'>{JSON.stringify(starWarsData,null,2)}</pre>
+                <h1 className='api_title'>The count is {count}</h1>
+                <button onClick={()=>setCount(prevCount=>prevCount+1)}>Add</button>
             </div>
         </section>
     )

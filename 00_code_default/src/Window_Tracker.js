e03b57262a9b1d624213099aed4c56e4fcd5bc0d
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-03-31 11:05:16 -0300

    up

--- 00_code_default/src/Window_Tracker.js
+++ 00_code_default/src/Window_Tracker.js
@@ -2,12 +2,25 @@ import React from 'react'
 
 export default function Window_Tracker(){
 
-    const [screen, setScreen] = React.useState(window.innerWidth)
+    const[windowWidth, setWindowWidth] = React.useState(window.innerWidth)
 
-    React.useEffect
+
+    React.useEffect(() =>{
+        function watchWidth(){
+            console.log('Setting up..')
+            setWindowWidth(window.innerWidth)
+        }
+
+        window.addEventListener('resize', watchWidth)
+        
+        return function(){
+            console.log('Cleaning up...')
+            window.removeEventListener('resize', watchWidth)
+        }
+    },[])
 
 
     return(
-        <h1 className='tracker_title'>Window width: {screen}px</h1>
+        <h1 className='tracker_title'>Window width: {windowWidth}px</h1>
     )
 }

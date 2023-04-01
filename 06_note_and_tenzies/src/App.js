c98cbe03b442095324c637bb49bf5627a7fc29d3
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-04-01 16:52:22 -0300

    up

--- 06_note_and_tenzies/src/App.js
+++ 06_note_and_tenzies/src/App.js
@@ -6,10 +6,18 @@ import Split from "react-split"
 import {nanoid} from "nanoid"
 
 export default function App() {
-    const [notes, setNotes] = React.useState([])
+    const [notes, setNotes] = React.useState(
+        ()=>JSON.parse(localStorage.getItem("notes")) || [])
+
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
+
+    React.useEffect(()=>{
+        localStorage.setItem("notes", JSON.stringify(notes))
+    },[notes])
+
+    const notes_title = (notes[0].body.split("\n")[0])
     
     function createNewNote() {
         const newNote = {
